50a7b943b8f167df85cba42d4e56d691067ef6a7
aayahda <aadhyakraj@gmail.com>
2023-10-02 12:20:49 +0530

    random sequence

--- game.js
+++ game.js
@@ -1,1 +1,13 @@
-alert("hi");
+var gamePattern=[];
+var buttonColors=["red","blue","green","yellow"]
+
+function nextSequence(){
+    randomNumber=Math.floor(Math.random()*4);
+    return randomNumber;
+}
+
+var randomChosenColor=buttonColors[nextSequence()];
+console.log(randomChosenColor);
+gamePattern.push(randomChosenColor);
+
+console.log(gamePattern);
