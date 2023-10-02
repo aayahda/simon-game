7a3ca7b285a2c6c08217fc11e7175e98d46699c6
aayahda <aadhyakraj@gmail.com>
2023-10-02 15:26:37 +0530

    sounds added

--- game.js
+++ game.js
@@ -2,21 +2,26 @@ var gamePattern=[];
 var buttonColors=["red","blue","green","yellow"]
 var userClickedPattern=[]
 
+$("btn").click(function(){
+    var userChosenColor=$(this).attr("id");
+    userClickedPattern.push(userChosenColor);
+   playSound(userChosenColor);
+});
+
 function nextSequence(){
     var randomNumber=Math.floor(Math.random()*4);
     var randomChosenColor=buttonColors[randomNumber];
     gamePattern.push(randomChosenColor);
     $("#"+randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
-    var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
-    audio.play();
+    playSound(randomChosenColor);
 
 }
 
-$("btn").click(function(){
-    var userChosenColor=$(this).attr("id");
-    userClickedPattern.push(userChosenColor);
-  });
 
 
 
+function playSound(name){
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+}
 
