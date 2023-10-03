63534a67d30883013a97554fb1aaebefe3cc55ac
aayahda <aadhyakraj@gmail.com>
2023-10-03 20:04:39 +0530

    final

--- game.js
+++ game.js
@@ -67,6 +67,14 @@ function checkAnswer(currentLevel){
         $("body").removeClass("game-over");
       }, 200);
       $("#level-title").text("Game Over, Press Any Key to Restart");
+      startOver();
     }
+
+}
+
+function startOver(){
+    level = 0;
+  gamePattern = [];
+  started = false;
 
 }
